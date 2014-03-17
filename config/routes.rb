c37f046919cc09fc665189b72c0e5ab213a1e611
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-17 11:20:12 +0000

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Sushi::Application.routes.draw do
+  root "pages#home"
+  
+  # get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
