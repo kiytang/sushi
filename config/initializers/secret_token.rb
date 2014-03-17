7e6ae64baafee432087d56f908133bf0f333a874
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-17 10:03:27 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sushi::Application.config.secret_key_base = '21e29ec8bef4c0eb6a703f9e8836f3e9b167e16132fc10504c0eb1518c8a78aee2073e76046f1dc022efc96b3a29cf5aa973815f624cb8949d017b1099c86127'
