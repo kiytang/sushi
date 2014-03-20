9976f4d16b0537d0cd38d2a285d756d924a26f0c
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-20 17:36:49 +0000

    added Figaro gem

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.2.4'
+gem 'figaro'
 
 group :development, :test do
   gem 'sqlite3'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Sushi::Application.config.secret_key_base = '21e29ec8bef4c0eb6a703f9e8836f3e9b167e16132fc10504c0eb1518c8a78aee2073e76046f1dc022efc96b3a29cf5aa973815f624cb8949d017b1099c86127'
+Sushi::Application.config.secret_key_base = ENV['SECRET_TOKEN']
