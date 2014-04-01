d88d30e85e0b70c1a37e5d0616a706d90db9d023
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-01 17:18:51 +0100

    index views loaded in reverse order

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,7 +6,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order(created_at: :desc)
   end
 
   # GET /pins/1
