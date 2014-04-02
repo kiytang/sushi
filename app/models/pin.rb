486699dd884524f8c3af3ed2ca7e073db53fe546
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-02 09:45:28 +0100

    added custom validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/sushi.png"
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
